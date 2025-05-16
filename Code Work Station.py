21a7d768fdb080ab94c0143e2c60f30ef71788f2
Scribeshock <tjsmith8518@gmail.com>
2025-05-16 15:06:00 -0500

    Delete Code Work Station.py

--- Code Work Station.py
+++
@@ -1,8 +0,0 @@
-try:
-    year = int(input("Enter The Year: "))
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 ==0):
-        print(" It is a leap year!")
-    else:
-        print(" It is not a leap year!")
-except ValueError:
-    print("Enter only Numbers.")
